27dc1071aa90145a91d9b58812b92025fa84d9da
Jacob Roling <jroling.dev@protonmail.com>
2023-07-13 15:24:39 +0930

    chore: savepoint

--- build.config.ts
+++ build.config.ts
@@ -1,17 +1,9 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  entries: [
-    // default
-    "./src/index",
-    // mkdist builder transpiles file-to-file keeping original sources structure
-    {
-      builder: "mkdist",
-      input: "./src/commands",
-      outDir: "./dist/commands",
-    },
-  ],
+  entries: ["./src/index", "./src/commands"],
   outDir: "dist",
+  clean: true,
   declaration: true,
   rollup: {
     emitCJS: true,
